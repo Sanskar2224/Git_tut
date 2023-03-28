fe2c8db469ac3e06f167d151ba9dd8f0f4be3123
Sanskar Singh <shansingh0015@gmail.com>
2023-03-29 00:10:38 +0530

    new file creation

---
+++ 1.java
@@ -0,0 +1,11 @@
+public class multivalue {
+    public static void main(String[] args){
+        int x=10;  
+        int y=25;
+        System.out.println("The value of x is ");
+        System.out.println(x);
+        System.out.println("The value of y is ");
+        System.out.println(y);
+    }
+    
+}
